747311a6665ae6a55450e700c819be6b98c15847
MohammadHishamm <mohammad2109652@miuegypt.edu.eg>
2024-11-28 21:07:34 +0200

    python methods count fix

--- APP/out/Metrics/MethodCountMetric.js
+++ APP/out/Metrics/MethodCountMetric.js
@@ -5,17 +5,19 @@ const MetricCalculator_1 = require("../Core/MetricCalculator");
 class MethodCountMetric extends MetricCalculator_1.MetricCalculator {
     calculate(node) {
         let methodCount = 0;
-        const traverse = (currentNode) => {
-            if (currentNode.type === 'method_declaration') {
+        const traverse = (currentNode, parentNodeType) => {
+            if ((currentNode.type === 'method_declaration') || // For Java
+                (currentNode.type === 'function_definition') // For Python
+            ) {
                 methodCount++;
             }
             if (currentNode.children) {
                 for (const child of currentNode.children) {
-                    traverse(child); // Recursively traverse child nodes
+                    traverse(child, currentNode.type); // Pass parent node type
                 }
             }
         };
-        traverse(node);
+        traverse(node, null);
         return methodCount;
     }
 }
--- APP/src/Metrics/MethodCountMetric.ts
+++ APP/src/Metrics/MethodCountMetric.ts
@@ -4,19 +4,22 @@ export class MethodCountMetric extends MetricCalculator {
     calculate(node: any): number {
         let methodCount = 0;
 
-        const traverse = (currentNode: any) => {
-            if (currentNode.type === 'method_declaration') {
+        const traverse = (currentNode: any, parentNodeType: string | null) => {
+            if (
+                (currentNode.type === 'method_declaration') || // For Java
+                (currentNode.type === 'function_definition') // For Python
+            ) {
                 methodCount++;
             }
+
             if (currentNode.children) {
                 for (const child of currentNode.children) {
-                    traverse(child); // Recursively traverse child nodes
+                    traverse(child, currentNode.type); // Pass parent node type
                 }
             }
-            
         };
 
-        traverse(node);
+        traverse(node, null);
         return methodCount;
     }
 }
