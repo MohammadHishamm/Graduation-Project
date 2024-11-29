559a444e29c5f1617d4d5f68853e709d25777e2f
zeyad zayaty <zeyadzayaty@gmail.com>
2024-11-29 16:41:44 +0200

    #python metrics folder small edits

--- APP/src/Metrics/Python/PythonCognitivecomplexty.ts
+++ APP/src/Metrics/Python/PythonCognitivecomplexty.ts
@@ -14,10 +14,10 @@ export class PythonCyclomaticComplexityMetric extends MetricCalculator {
                     'for_statement',    // for 'for'
                     'while_statement',  // for 'while'
                     'with_statement',   // for 'with'
-                    'except_clause',     // for 'catch'
+                    'except_clause',  // for 'catch'
+                    'match ',   // for 'switchcase'
                     'raise_statement',  // for 'throw'
-                    'break_statement', // for 'break'
-                    'case',  // for 'cases in the switch case'
+                    'break_statement',  // for 'break'
                     'continue_statement'// for 'continue'
                 ].includes(currentNode.type)
             ) {
@@ -27,13 +27,6 @@ export class PythonCyclomaticComplexityMetric extends MetricCalculator {
             if(currentNode.type === 'function_definition')
             {
                 complexity++;
-            }
-
-            // Increment for boolean operators (&&, ||)
-            if (currentNode.type === 'and' || currentNode.type === 'or') {
-             
-                    complexity++;
-                
             }
 
             // Recursively traverse child nodes
