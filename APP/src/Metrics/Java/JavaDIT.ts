0cd97cbdd7f78b070c95fb80c7f2d5561fa0e471
Mark <mark2107794@miuegypt.edu.eg>
2025-01-26 18:36:46 +0200

    findDITFunction

--- APP/src/Metrics/Java/JavaDIT.ts
+++ APP/src/Metrics/Java/JavaDIT.ts
@@ -19,5 +19,45 @@ export class DepthOfInheritanceTree extends MetricCalculator {
     return this.findDIT(Classes, node, FECFC);
   }
 
-  
+  private findDIT(
+    Classes: ClassInfo[],
+    rootNode: Parser.SyntaxNode,
+    FECFC: FolderExtractComponentsFromCode
+): number {
+    let DIT = 0;
+    let isExtended; // To track the extended class
+    
+    const fileParsedComponents = FECFC.getParsedComponentsFromFile(); // Get all parsed file components
+
+    // Loop through Classes to identify the extended class
+    for (const c of Classes) {
+        isExtended = c.extendedclass; // The class that extends another class
+    }
+
+    // If the class has an extended class (i.e., it's not a root class)
+    if (isExtended) {
+        // Loop through the parsed components of the file
+        for (const fileComponents of fileParsedComponents) {
+            // Loop through class groups to find matching extended class
+            for (const classGroup of fileComponents.classes) {
+                if (isExtended === classGroup.name) {
+                    // If the class name matches the extended class, increment DIT
+                    DIT++;
+                    
+                    // Recursively traverse through subclasses (if they exist)
+                    for (const classInfo of classGroup.classes) {
+                        if (classInfo.extendedclass) {
+                            console.log(classInfo);
+                            // Recursively call findDIT to calculate DIT for subclass
+                            DIT += this.findDIT([classInfo], rootNode, FECFC);
+                            
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return DIT;
+}
 }
